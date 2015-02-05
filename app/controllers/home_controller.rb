dce3b49d7d17f00e1fecac061a6e84e2c7a72087
Petro <petro.yakubiv@gmail.com>
2015-02-05 21:41:49 +0200

    Index action removed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-	  http = Keen.publish_async(:customers, { first_name: "lox", :referred_by => "harry" })
-		http.callback { |response| puts "Success: #{response}"}
-		http.errback { puts "was a failurrr :,(" }
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,5 +11,5 @@ Rails.application.configure do
   config.assets.digest = true
 
   config.assets.raise_runtime_errors = true
-
+  
 end
